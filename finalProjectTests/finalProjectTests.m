77e311516f71bf29ab2573f876558e8b016df8de
iD Student <Student@idtech-id06.princeton.edu>
2012-07-02 11:04:37 -0700

    Initial Commit

---
+++ finalProjectTests/finalProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  finalProjectTests.m
+//  finalProjectTests
+//
+//  Created by iD Student on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "finalProjectTests.h"
+
+@implementation finalProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in finalProjectTests");
+}
+
+@end
